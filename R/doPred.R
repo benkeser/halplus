11e1415c903d4b69dc8b2b6df13b4fd35207a9c6
Chris Kennedy <chrisken@gmail.com>
2016-10-04 11:18:49 -0700

    Reformat code using RStudio

--- R/doPred.R
+++ R/doPred.R
@@ -1,43 +1,50 @@
 #' @importFrom Matrix sparseMatrix
-doPred <- function(object,newdata,verbose=TRUE,s) {
-  if(is.vector(newdata)) newdata <- matrix(newdata)
+doPred <- function(object, newdata, verbose = TRUE, s) {
+  if (is.vector(newdata))
+    newdata <- matrix(newdata)
 
-  if(verbose) cat("Making initial sparse matrix for predictions \n")
-  tmp <- makeSparseMat(X=object$X, newX=newdata, verbose=verbose)
+  if (verbose)
+    cat("Making initial sparse matrix for predictions \n")
+  tmp <- makeSparseMat(X = object$X,
+                       newX = newdata,
+                       verbose = verbose)
 
-  if(length(object$dupInds) > 0){
-    if(verbose) cat("Correcting duplicate columns in sparse matrix \n")
+  if (length(object$dupInds) > 0) {
+    if (verbose)
+      cat("Correcting duplicate columns in sparse matrix \n")
     # get vector of duplicate columns
-    dupVec <- unlist(object$colDups,use.names=FALSE)
+    dupVec <- unlist(object$colDups, use.names = FALSE)
     # number of each duplicate
-    nperDup <- unlist(lapply(object$colDups,length),use.names=FALSE)
+    nperDup <- unlist(lapply(object$colDups, length), use.names = FALSE)
     # number of duplicates to roll through
     K <- length(nperDup)
     # start and ending index
-    startInd <- c(0, cumsum(nperDup)[1:(K-1)])
+    startInd <- c(0, cumsum(nperDup)[1:(K - 1)])
     endInd <- cumsum(nperDup)
     # not duplicate colums
     notdupVec <- (1:ncol(tmp))[-dupVec]
     # put all the duplicated guys first
-    tmp <- tmp[,c(dupVec,notdupVec)]
+    tmp <- tmp[, c(dupVec, notdupVec)]
 
     uniqRowsList <- list()
-    myp <- c(0,rep(NA, K))
+    myp <- c(0, rep(NA, K))
     # look at the i associatiated with
-    for(k in 1:K){
+    for (k in 1:K) {
       # this condition ensures that not all the values of a given set of duplciates
       # are equal to zero.
-      if(tmp@p[startInd[k]+1] != tmp@p[endInd[k]+1]){
-        Fidx_base0 <- (tmp@p[startInd[k] + 1]) : (tmp@p[endInd[k] + 1] - 1)
-        nonzero_rows <- tmp@i[Fidx_base0 + 1] + 1 # actual row numbers of non-zero elements in column i=1
+      if (tmp@p[startInd[k] + 1] != tmp@p[endInd[k] + 1]) {
+        Fidx_base0 <- (tmp@p[startInd[k] + 1]):(tmp@p[endInd[k] + 1] - 1)
+        nonzero_rows <-
+          tmp@i[Fidx_base0 + 1] + 1 # actual row numbers of non-zero elements in column i=1
         # unique nonzero_rows
         theseRows <- sort(unique(nonzero_rows))
         uniqRowsList[[k]] <- theseRows
         # a new p vector for duplicated columns
-        myp[k+1] <- myp[k] + length(theseRows)
-      }else{ # the whole block for this set of duplicates is 0
+        myp[k + 1] <- myp[k] + length(theseRows)
+      } else{
+        # the whole block for this set of duplicates is 0
         uniqRowsList[[k]] <- NULL
-        myp[k+1] <- myp[k]
+        myp[k + 1] <- myp[k]
       }
     }
 
@@ -49,23 +56,28 @@ doPred <- function(object,newdata,verbose=TRUE,s) {
     # fix up p with nondup columns
     ## for this example every non-duplicated column in the new design
     ## matrix is 0, which is causing this to break. I think.
-    if(tmp@p[endInd[K] + 1] != tmp@p[length(tmp@p)]){
-      fulli <- c(myi, tmp@i[(tmp@p[endInd[K] + 1] + 1) : tmp@p[length(tmp@p)]] + 1)
+    if (tmp@p[endInd[K] + 1] != tmp@p[length(tmp@p)]) {
+      fulli <-
+        c(myi, tmp@i[(tmp@p[endInd[K] + 1] + 1):tmp@p[length(tmp@p)]] + 1)
       fullp <- c(myp,
-                 tmp@p[((endInd[K]+1) + 1) : length(tmp@p)] -
-                   tmp@p[(endInd[K]+1)] + myp[K+1])
-    }else{
+                 tmp@p[((endInd[K] + 1) + 1):length(tmp@p)] -
+                   tmp@p[(endInd[K] + 1)] + myp[K + 1])
+    } else{
       fulli <- myi
       fullp <- myp
     }
     #
-    tmp <- Matrix::sparseMatrix(i=fulli, p=fullp, x=1,
-                        dims=c(length(newdata[,1]),
-                               length(notdupVec) + length(object$colDup)))
+    tmp <- Matrix::sparseMatrix(
+      i = fulli,
+      p = fullp,
+      x = 1,
+      dims = c(
+        length(newdata[, 1]),
+        length(notdupVec) + length(object$colDup)
+      )
+    )
   }
-  pred <- predict(object$object$glmnet.fit, newx=tmp,
+  pred <- predict(object$object$glmnet.fit, newx = tmp,
                   s = s)
   pred
 }
-
-
--- R/makeSparseMat.R
+++ R/makeSparseMat.R
@@ -1,13 +1,15 @@
 #' @importFrom plyr llply
-makeSparseMat <- function(X,newX=X, verbose=TRUE){
-  if(is.vector(X)) X <- matrix(X, ncol=1)
-  if(is.vector(newX)) newX <- matrix(newX, ncol=1)
+makeSparseMat <- function(X, newX = X, verbose = TRUE) {
+
+  if (is.vector(X)) X <- matrix(X, ncol = 1)
+
+  if (is.vector(newX)) newX <- matrix(newX, ncol = 1)
 
   d <- ncol(X)
-  stopifnot(ncol(X)==ncol(newX))
+  stopifnot(ncol(X) == ncol(newX))
 
-  nX <- length(X[,1])
-  n <- length(newX[,1])
+  nX <- length(X[, 1])
+  n <- length(newX[, 1])
 
   # numbers used to correct column indices later
   colStart <- 1
@@ -19,91 +21,98 @@ makeSparseMat <- function(X,newX=X, verbose=TRUE){
   # only on that covariate, i.e. columns in each list entry
   # run from 1:n, so we can use intersect later for higher
   # order terms.
-  if(verbose) cat("Making ", d," basis fns of dimension  1 \n")
-  uni <- alply(matrix(1:d),1,function(x){
-    j <- alply(matrix(newX[,x]),1,function(y){
-      which(X[,x] <= y)
+  if (verbose) cat("Making ", d, " basis fns of dimension  1 \n")
+
+  uni <- alply(matrix(1:d), 1, function(x) {
+    j <- alply(matrix(newX[, x]), 1, function(y) {
+      which(X[, x] <= y)
     })
-    i <- rep(1:n,unlist(lapply(j, length), use.names=FALSE))
-    cbind(unlist(i, use.names=FALSE),unlist(j, use.names=FALSE))
+    i <- rep(1:n, unlist(lapply(j, length), use.names = FALSE))
+    cbind(unlist(i, use.names = FALSE), unlist(j, use.names = FALSE))
   })
   # number of 1's for each variable -- for variables with
   # length(unique(x)) == length(x) will be n*(n+1)/2, but if there
   # are ties, the length will be different
-  nperuni <- lapply(uni,nrow)
+  nperuni <- lapply(uni, nrow)
 
   # slap them all together
-  uni.ij <- Reduce("rbind",uni)
+  uni.ij <- Reduce("rbind", uni)
 
   # fix up the column indices
-  uni.ij[,2] <- uni.ij[,2] +
-    rep.int((colStart:colEnd)-1, times=unlist(nperuni, use.names=FALSE))*nX
+  uni.ij[, 2] <- uni.ij[, 2] +
+    rep.int((colStart:colEnd) - 1, times = unlist(nperuni, use.names = FALSE)) *
+    nX
 
   # i = row indices, j = column indices
-  i <- uni.ij[,1]
-  j <- uni.ij[,2]
+  i <- uni.ij[, 1]
+  j <- uni.ij[, 2]
 
   # functions used for higher order terms
-  .myIntersect <- function(...){
-    Reduce(intersect,list(...))
+  .myIntersect <- function(...) {
+    Reduce(intersect, list(...))
   }
-  .getIntersect <- function(...){
-    tmp <- lapply(..., function(b){
-      split(b[,2],b[,1])
+
+  .getIntersect <- function(...) {
+    tmp <- lapply(..., function(b) {
+      split(b[, 2], b[, 1])
     })
-    tmpNames <- lapply(tmp,function(l){
+    tmpNames <- lapply(tmp, function(l) {
       as.numeric(names(l))
     })
-    overlap <- Reduce(intersect,tmpNames)
+    overlap <- Reduce(intersect, tmpNames)
 
     # indices of tmp that overlap
-    newtmp <- lapply(tmp,function(b){
+    newtmp <- lapply(tmp, function(b) {
       b[paste(overlap)]
     })
 
     # get intersection
-    out <- eval(parse(text=paste0(paste0(
-      "mapply(.myIntersect,"),paste0("newtmp[[",1:length(tmp),"]]",collapse=","),",SIMPLIFY=FALSE)")
-    ))
+    out <- eval(parse(text = paste0(
+      paste0("mapply(.myIntersect,"),
+      paste0("newtmp[[", 1:length(tmp), "]]", collapse = ","),
+      ",SIMPLIFY=FALSE)"
+    )))
     out
   }
 
   # loop over higher order terms
-  if(d > 1){
-    for(k in 2:d){
+  if (d > 1) {
+    for (k in 2:d) {
       # matrix of all d choose k combinations
-      combos <- combn(d,k)
+      combos <- combn(d, k)
 
-      if(verbose) cat("Making ", ncol(combos), " basis fns of dimension ", k, "\n" )
+      if (verbose)
+        cat("Making ", ncol(combos), " basis fns of dimension ", k, "\n")
       # adjust column indicators for column indices
       colStart <- colEnd + 1
-      colEnd <- (colStart-1) + ncol(combos)
+      colEnd <- (colStart - 1) + ncol(combos)
 
       # list of length d choose k, each entry
       # containing n indices of columns corresponding to subjects
-      j.list <- alply(combos, 2, function(a){
+      j.list <- alply(combos, 2, function(a) {
         .getIntersect(uni[a])
       })
 
       # list of length d choose k, each entry containing
       # n indices of rows corresponding to subjects
-      i.list <- plyr::llply(j.list, function(x){
-        rep(as.numeric(names(x)), unlist(lapply(x, length), use.names=FALSE))
+      i.list <- plyr::llply(j.list, function(x) {
+        rep(as.numeric(names(x)), unlist(lapply(x, length), use.names = FALSE))
       })
 
       # number of 1's for each combination
       nper <- lapply(i.list, length)
 
       # unlist column numbers
-      j.list <- lapply(j.list, unlist, use.names=FALSE)
+      j.list <- lapply(j.list, unlist, use.names = FALSE)
 
       # unlist rows and columns
-      thisi <- unlist(i.list,use.names=FALSE)
-      thisj <- unlist(j.list,use.names=FALSE)
+      thisi <- unlist(i.list, use.names = FALSE)
+      thisj <- unlist(j.list, use.names = FALSE)
 
       # fix up the column number
       thisj <- thisj +
-        rep.int((colStart:colEnd)-1, times=unlist(nper, use.names=FALSE))*nX
+        rep.int((colStart:colEnd) - 1, times = unlist(nper, use.names =
+                                                        FALSE)) * nX
 
       # put it together
       i <- c(i, thisi)
@@ -112,6 +121,12 @@ makeSparseMat <- function(X,newX=X, verbose=TRUE){
   }
 
   # make the sparseMatrix
-  grbg <- Matrix::sparseMatrix(i=i[order(i)],j=j[order(i)],x=1, dims=c(n, nX*(2^d - 1)))
+  grbg <-
+    Matrix::sparseMatrix(
+      i = i[order(i)],
+      j = j[order(i)],
+      x = 1,
+      dims = c(n, nX * (2 ^ d - 1))
+    )
   return(grbg)
 }
